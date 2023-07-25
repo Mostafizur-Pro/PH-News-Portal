d9802af4f826f7d6133c2d77c79f4cbdd9a33027
Mostafizur-Pro <mostafizur0195@gmail.com>
2023-07-25 16:31:10 +0600

    news page create

--- src/components/UI/AllNews.js
+++ src/components/UI/AllNews.js
@@ -1,27 +1,79 @@
 import React from "react";
-import { Card } from "antd";
-const { Meta } = Card;
+import { Button, Card, Col, Row } from "antd";
+import {
+  ArrowRightOutlined,
+  CalendarOutlined,
+  CommentOutlined,
+  ProfileOutlined,
+} from "@ant-design/icons";
+import Image from "next/image";
+// import Link from "next/link";
+
+// const contentStyle = {
+//   height: "200px",
+//   width: "200 px",
+//   color: "#000",
+// };
 
 function AllNews({ allNews }) {
-  //   console.log(allNews);
-  //   const { image_url, title, author, category, coment_count, id, description } =
-  //     allNews;
+  console.log(allNews);
   return (
     <>
-      {allNews.map((news) => {
-        <Card
-          hoverable
-          style={{
-            width: 240,
-          }}
-          cover={<img alt="news" src={news.image_url} />}
-        >
-          <Meta
-            title={title}
-            description={`Total Comments :${news.comment_count}`}
-          />
-        </Card>;
-      })}
+      <Row gutter={16}>
+        {allNews.map((news) => (
+          <>
+            {/* <Col style={contentStyle}>
+              <Image src={news.image_url} fill alt="drawing_image" />
+            </Col> */}
+
+            <Col span={6}>
+              <Card
+                hoverable
+                style={{ width: 240 }}
+                cover={<img alt="newsImage" src={news.image_url} />}
+              ></Card>
+
+              <h1 style={{ fontSize: "20px" }}>{news.title}</h1>
+              <div
+                className="line"
+                style={{
+                  height: "5px",
+                  margin: "20px 0",
+
+                  background: "#000",
+                  width: "95%",
+                }}
+              ></div>
+
+              <p
+                style={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  width: "90%",
+                  color: "gray",
+                  margin: "10px 0px",
+                }}
+              >
+                <span>
+                  <CalendarOutlined /> {news.release_date}
+                </span>
+                <span>
+                  <CommentOutlined /> {news.comment_count}
+                </span>
+                <span>
+                  <ProfileOutlined /> {news.category}
+                </span>
+              </p>
+
+              <p style={{ fontSize: "20px" }}>{news.description}</p>
+
+              <Button type="primary">
+                Keep Reading <ArrowRightOutlined />
+              </Button>
+            </Col>
+          </>
+        ))}
+      </Row>
     </>
   );
 }
