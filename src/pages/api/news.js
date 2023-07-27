7670b6d7ca4dc6244193741133e123a447e0f9fd
Mostafizur-Pro <mostafizur0195@gmail.com>
2023-07-27 15:54:38 +0600

    mongodb server add

---
+++ src/pages/api/news.js
@@ -0,0 +1,29 @@
+// User: ph-news   pass: 91ABzrlhz3yXrnLm
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://ph-news:91ABzrlhz3yXrnLm@cluster0.5xecsyp.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run(req, res) {
+  try {
+    await client.connect();
+
+    const newsCollection = client.db("ph-news").collection("news");
+
+    if (req.method === "GET") {
+      const news = await newsCollection.find({}).toArray();
+      res.send({ message: "Success", statur: 200, data: news });
+    }
+  } finally {
+  }
+}
+// run().catch(console.dir);
+export default run;
